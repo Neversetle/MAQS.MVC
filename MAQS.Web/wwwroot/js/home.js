e7879abdc202e4d0cc0b6a30b6b01cccd137dc41
srinivas <srinivasmaa.in@mouritech.com>
2022-10-11 16:52:40 +0530

    comitting the js changes

---
+++ MAQS.Web/wwwroot/js/home.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('#tblCorp').DataTable(
+        {
+            ajax: {
+                url: "Home/GetEmployeeList",
+                type: "POST",
+            },
+            processing: true,
+            serverSide: true,
+            filter: true,
+            columns: [
+                { data: "employeeId", name: "EmployeeId" },
+                { data: "employeeFirstName", name: "EmployeeFirstName" },
+                { data: "employeeLastName", name: "EmployeeLastName" },
+                { data: "salary", name: "Salary" },
+                { data: "designation", name: "Designation" },
+            ]
+        }
+    );
+});
